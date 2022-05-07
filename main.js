3073230dfd2aa98c2b6a32b202f085e30138fb14
KuwarbirSK <88274899+KuwarbirSK@users.noreply.github.com>
2022-05-07 10:32:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,50 +1,17 @@
-var names_of_people = [];
-    
-function submit()
-{
-    var GuestName = document.getElementById("name1").value;
-	// use the push function as - names_of_people.push(GuestName);
-	names_of_people.push(GuestName);
-	console.log(GuestName);    
-    console.log(names_of_people);
-    var lenght_of_name = names_of_people.length;
-    console.log(lenght_of_name);
-	document.getElementById("display_name").innerHTML=names_of_people.toString();
-   }
-
-
-
-function show()
-{
-	var i= names_of_people.join("<br>");
-	console.log(names_of_people);
-	document.getElementById("p1").innerHTML=i.toString();
-	document.getElementById("sort_button").style.display="block";
-	
+function getParagraph1(){
+    var inputs = [];
+    for(var i = 1 ; i <=6 ; i++){
+        inputs.push(document.getElementById("para1_input" + i).value);
+    }
+    inputs.join(". ");
+    document.getElementById("showPara1").innerHTML = inputs.join(". ");
 }
 
-
-function sorting()
-	{
-		names_of_people.sort;           // add the sort function here
-		// .sort();
-		var i= names_of_people.join("<br>");
-		console.log(names_of_people);		
-		document.getElementById("sorted").innerHTML=i.toString();
-		}
-
-
-function searching()
-{
-	var s= document.getElementById("s1").value;
-	var found=0;
-	var j;
-	for(j=0; j<names_of_people.length; j++)
-		{
-			if(s==names_of_people[j]){
-				found=found+1;
-			}	
-		}
-	document.getElementById("p2").innerHTML="name found "+found+" time/s";
-	console.log("found name "+found+" time/s");
+function getParagraph2(){
+    var inputs1 = [];
+    for(var j = 1 ; j<=6 ; j++){
+        inputs1.push(document.getElementById("para2_input" + j).value);
+    }
+    inputs1.join(". ");
+    document.getElementById("showPara2").innerHTML = inputs1.join(". ");
 }
